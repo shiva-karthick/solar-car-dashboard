53dbcf2f81af1965637d45ad539afd2263ada7f4
shankaarshiva <email@example.com>
2018-08-03 01:50:29 +0800

    Demo to push to Github via Visual Studio

--- arduinoSensors/3 separate demos/battery/battery.ino
+++ arduinoSensors/3 separate demos/battery/battery.ino
@@ -1,14 +1,14 @@
-int batteryPinNumber = A0;    // select the input pin for the potentiometer
-float batteryValue = 75;  // variable to store the value coming from the sensor
-
 void setup() {
-  // declare the ledPin as an OUTPUT:
+  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
 }
 
 void loop() {
-  // read the value from the sensor:
-  batteryValue = analogRead(batteryPinNumber);
-  Serial.print(batteryValue);
-  delay(1000); // for stability
+  // read the input on analog pin 0:
+  float batteryValue = analogRead(A0);
+  float value = (batteryValue/1023) * 5;
+  float realValue = value * 30;
+  // print out the value you read:
+  Serial.println(realValue);
+  delay(1);        // delay in between reads for stability
 }
